1bb10405415aafb06aab4580be543fc639800614
Luis Castro <castroluis1694@gmail.com>
2026-07-23 01:42:04 -0300

    Updates

---
+++ i18n/config.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationES from './es.json';
+import translationEN from './en.json';
+
+const resources = {
+  en: { translation: translationEN },
+  es: { translation: translationES }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'es', 
+    fallbackLng: 'es',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
